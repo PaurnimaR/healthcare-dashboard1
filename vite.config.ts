f26f68e21ea81f830710a14881b4c86ff67e1d5e
paurnimaarashinkar <paurnimaarashinkar@gmail.com>
2025-05-28 18:41:40 +0530

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/healthcare-dashboard/', // 👈 Add this
+  base: '/healthcare-dashboard/', // 👈 Must match your repo name
   plugins: [react()],
-})
+});
